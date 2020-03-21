65aeae85935e88c993b9a9cdec6568bfd02deda7
sourav5360 <40808184+sourav5360@users.noreply.github.com>
2020-03-20 21:42:59 -0400

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,73 @@
+#include <SPI.h>
+
+#include <Wire.h>
+
+#include <Adafruit_GFX.h>
+
+#include <Adafruit_SSD1306.h>
+
+#define OLED_RESET 4
+
+Adafruit_SSD1306 display(OLED_RESET);
+
+void setup() {
+
+Serial.begin(9600);
+
+display.begin(SSD1306_SWITCHCAPVCC, 0x3D);
+
+display.display();
+
+delay(2000);
+
+display.clearDisplay();
+
+}
+
+void loop() {
+
+while(Serial.available() > 0){
+
+String Date = Serial.readStringUntil('|');
+
+Serial.read();
+
+String Time = Serial.readStringUntil('|');
+
+Serial.read();
+
+String Phone = Serial.readStringUntil('|');
+
+Serial.read();
+
+String Text = Serial.readStringUntil('\n');
+
+Serial.read();
+
+}
+
+if(Text == "text" && Phone == "phone")
+
+{ display.println(Date);
+
+display.display();
+
+display.println(Time);
+
+display.display();
+
+display.clearDisplay();
+
+}
+
+if (Text != "text" && Phone == "phone"){
+
+display.println(Text);
+
+display.display();
+
+delay(5000);
+
+display.clearDisplay();
+
+}
