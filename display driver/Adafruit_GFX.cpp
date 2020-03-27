c160271e5ffb9f192b8ba4ad236858e9267b6186
gursewakkharoud95 <47992157+gursewakkharoud95@users.noreply.github.com>
2020-03-26 23:35:27 -0400

    Update Adafruit_GFX.cpp

--- display driver/Adafruit_GFX.cpp
+++ display driver/Adafruit_GFX.cpp
@@ -1,28 +1,3 @@
-/******************************************************************
- This is the core graphics library for all our displays, providing
- basic graphics primitives (points, lines, circles, etc.). It needs
- to be paired with a hardware-specific library for each display
- device we carry (handling the lower-level functions).
- 
- Adafruit invests time and resources providing this open
- source code, please support Adafruit and open-source hardware
- by purchasing products from Adafruit!
- 
- Written by Limor Fried/Ladyada for Adafruit Industries.
- BSD license, check license.txt for more information.
- All text above must be included in any redistribution.
- 
-02/18/2013 	Charles-Henri Hallard (http://hallard.me)
-						Modified for compiling and use on Raspberry ArduiPi Board
-						LCD size and connection are now passed as arguments on 
-						the command line (no more #define on compilation needed)
-						ArduiPi project documentation http://hallard.me/arduipi
-07/01/2013 	Charles-Henri Hallard (http://hallard.me)
-						Created Draw Bargraph feature
-						Added printf feature
-
- ******************************************************************/
-
 #include "./ArduiPi_SSD1306.h"
 #include "./Adafruit_GFX.h"
 #include "./glcdfont.c"
