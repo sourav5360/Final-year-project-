1e4b3841ea2ae24bbe34fb3784545e985ad1ff81
jaideeprajput <49762162+jaideeprajput@users.noreply.github.com>
2020-03-30 18:50:08 -0400

    Update ssd1306_i2c.c

--- main/source/ssd1306_i2c.c
+++ main/source/ssd1306_i2c.c
@@ -778,3 +778,28 @@ void ssd1306_drawChar(int x, int y, unsigned char c, int color, int size)
 		}
 	}
 }
+if ((x >= WIDTH) ||	// Clip right
+	    (y >= HEIGHT) ||	// Clip bottom
+	    ((x + 6 * size - 1) < 0) ||	// Clip left
+	    ((y + 8 * size - 1) < 0))	// Clip top
+		return;
+	int i;
+	int j;
+	for (i = 0; i < 6; i++) {
+		int line;
+		if (i == 5)
+			line = 0x0;
+		else
+			line = pgm_read_byte(font + (c * 5) + i);
+		for (j = 0; j < 8; j++) {
+			if (line & 0x1) {
+				if (size == 1)	// default size
+					ssd1306_drawPixel(x + i, y + j, color);
+				else {	// big size
+					ssd1306_fillRect(x + (i * size),
+							 y + (j * size), size,
+							 size, color);
+				}
+			}
+			line >>= 1;
+		}
