4957b256ccd66945070139e33294e3656487cfee
jaideeprajput <49762162+jaideeprajput@users.noreply.github.com>
2020-03-29 21:49:57 -0400

    Update respberry.c

--- main/source/respberry.c
+++ main/source/respberry.c
@@ -148,3 +148,22 @@ int main(int arg, char *args[]) {
 	ssd1306_display();
 }
 
+ssd1306_display(); //Adafruit logo is visible
+	ssd1306_clearDisplay();
+	delay(5000);
+
+	char* text = "This is demo for SSD1306 i2c driver for Raspberry Pi";
+	ssd1306_drawString(text);
+	ssd1306_display();
+	delay(5000);
+
+	ssd1306_dim(1);
+	ssd1306_startscrollright(00,0xFF);
+	delay(5000);
+
+	ssd1306_clearDisplay();
+	ssd1306_fillRect(10,10, 50, 20, WHITE);
+	ssd1306_fillRect(80, 10, 130, 50, WHITE);
+	ssd1306_display();
+}
+
