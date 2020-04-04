1ae0082b7ebdce98ffef7d1d7af0c3ebc557370a
jaideeprajput <49762162+jaideeprajput@users.noreply.github.com>
2020-04-03 23:21:59 -0400

    Update bluetoothcode.c

--- main/source/bluetoothcode.c
+++ main/source/bluetoothcode.c
@@ -1,1 +1,35 @@
+#include "ssd1306_i2c.h"
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <bluetooth/bluetooth.h>
+#include <bluetooth/rfcomm.h>
 
+#include <pthread.h>
+struct ps
+{
+	int st;
+	pthread_t *thr;
+
+int status = 0;
+void *recvsocket(void *arg)//
+{
+	struct ps *p = (struct ps *)arg;
+	int st = p->st;
+	char s[1024];
+ 
+	while(1)
+	{
+		memset(s, 0, sizeof(s));
+		//bytes_read = read(client, buf, sizeof(buf));
+		int rc = read(st, s, sizeof(s));
+		if (rc <= 0)//
+			break;
+		printf("phone：%s", s);
+		printf("Display OLED\n");
+		ssd1306_Display();
+		ssd1306_drawString(s);
+		ssd1306_display();
+		//delay(100);
+ 
+	}
