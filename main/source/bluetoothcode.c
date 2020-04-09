b567a8547397912a96771f033a7583e4ea3836fd
jaideeprajput <49762162+jaideeprajput@users.noreply.github.com>
2020-04-08 20:32:17 -0400

    Update bluetoothcode.c

--- main/source/bluetoothcode.c
+++ main/source/bluetoothcode.c
@@ -43,3 +43,34 @@ void *recvsocket(void *arg)//
 	return NULL;
 }
 	
+void *sendsocket(void *arg)
+{
+	int st = *(int *)arg;
+	char s[1024];
+	while(1)
+	{
+		memset(s, 0, sizeof(s));
+		read(STDIN_FILENO, s, sizeof(s));
+		write(st, s, strlen(s));
+	}
+	return NULL;
+}
+
+int main(int arg, char *args[]) {
+    struct sockaddr_rc loc_addr = { 0 }, rem_addr = { 0 };
+    char buf[1024] = { 0 };
+    int s, client, bytes_read;
+    socklen_t opt = sizeof(rem_addr);
+    //ssd1306 begin
+	ssd1306_begin(SSD1306_SWITCHCAPVCC, SSD1306_I2C_ADDRESS);
+    // allocate socket
+    s = socket(AF_BLUETOOTH, SOCK_STREAM, BTPROTO_RFCOMM);
+
+    // bind socket to port 1 of the first available 
+    // local bluetooth adapter
+    loc_addr.rc_family = AF_BLUETOOTH;
+    loc_addr.rc_bdaddr = *BDADDR_ANY;
+    loc_addr.rc_channel = (uint8_t) 1;
+    bind(s, (struct sockaddr *)&loc_addr, sizeof(loc_addr));
+
+    // put socket into listening 
